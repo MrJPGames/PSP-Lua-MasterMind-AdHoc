40955da0d7f6439f0600af72af4ef0070e11f32c
MrJPGames <jasper.peters@gmail.com>
2014-07-25 23:50:08 +0200

    Added more to render.lua
    Added rendering for win/lose screen
    moved image loading to render.lua
    moved pinx and piny init to render.lua
    made not yet set pins not render (New feature in script.lua)

--- functions/render.lua
+++ functions/render.lua
@@ -1,3 +1,20 @@
+px={}
+py={}
+px[0]=0
+px[1]=13
+px[2]=0
+px[3]=13
+py[0]=0
+py[1]=0
+py[2]=13
+py[3]=13
+
+--Load sprites and background
+sp=Image.load("sprites.png")
+bg=Image.load("bg.png")
+bgWin=Image.load("bgWin.png")
+bgGO=Image.load("bgGameOver.png")
+
 --Draws everything that was rendered to the screen
 function flip()
 	--Renders the screen
@@ -13,7 +30,9 @@ function renderGame()
 		for j=0,7,1 do
 			if j <= currow then
 				if c[j][i] < 3 then
-					screen:blit(9+27*i,246-j*30,sp,17*c[j][i],0,17,17,1)
+					if c[j][i] > -1 then
+						screen:blit(9+27*i,246-j*30,sp,17*c[j][i],0,17,17,1)
+					end
 				else
 					screen:blit(9+27*i,246-j*30,sp,17*(c[j][i]-3),17,17,17,1)
 				end
@@ -36,4 +55,12 @@ function renderGame()
 			end
 		end
 	end
+end
+
+function renderWin()
+	screen:blit(0,0,bgWin)
+end
+
+function renderGameOver()
+	screen:blit(0,0,bgGO)
 end
