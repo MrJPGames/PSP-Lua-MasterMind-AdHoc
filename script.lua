fa365d9f59a03bb5b0d97a64c2487cac390cc7dd
MrJPGames <jasper.peters@gmail.com>
2014-07-25 22:25:58 +0200

    added Graphics for game!
    Added the GUI for ingame!
    
    TODO:
    Add GUI Menu
    Add GUI Win/Lose screens!

--- script.lua
+++ script.lua
@@ -12,6 +12,21 @@ bpin={}
 wpin={}
 math.randomseed(os.time())
 
+px={}
+py={}
+px[0]=0
+px[1]=13
+px[2]=0
+px[3]=13
+py[0]=0
+py[1]=0
+py[2]=13
+py[3]=13
+
+--Load sprites and background
+sp=Image.load("sprites.png")
+bg=Image.load("bg.png")
+
 function flip()
 	--Renders the screen
 	screen.waitVblankStart()
@@ -115,6 +130,7 @@ while (1) do
 		end
 		
 		if pad:cross() and not oldpad:cross() then
+			st=0
 			for i=0,3,1 do
 				var1=c[currow][i]
 				var2=gc[i]
@@ -207,12 +223,33 @@ while (1) do
 		oldpad=pad
 		
 		--Render
-		screen:print(0,0,"Row: " .. currow,w)
-		screen:print(0,8,"Selected pin: " .. st,w)
-		screen:print(0,16,"Color of pin: " .. c[currow][st],w)
-		screen:print(0,24,"Good Color  : " .. gc[st],w)
-		if currow > 0 then
-			screen:print(0,32,"Black Pins: " .. bpin[currow-1] .. " White pins: " .. wpin[currow-1],w)
+		screen:blit(0,0,bg)
+		for i=0,3,1 do
+			for j=0,7,1 do
+				if j <= currow then
+					if c[j][i] < 3 then
+						screen:blit(9+27*i,246-j*30,sp,17*c[j][i],0,17,17,1)
+					else
+						screen:blit(9+27*i,246-j*30,sp,17*(c[j][i]-3),17,17,17,1)
+					end
+				end
+			end
+		end
+		screen:blit(13+27*st,242-currow*30,sp,20,38,9,4,1)
+		screen:blit(13+27*st,263-currow*30,sp,20,34,9,4,1)
+		--Render B/W pins
+		--rows
+		for j=0,7,1 do
+			if j < currow then
+			f=0
+				for i=0,bpin[j]-1,1 do
+					screen:blit(px[i]+122,py[i]+243-j*30,sp,10,34,10,10,1)
+					f=f+1
+				end
+				for i=0,wpin[j]-1,1 do
+					screen:blit(px[f+i]+122,py[f+i]+243-j*30,sp,0,34,10,10,1)
+				end
+			end
 		end
 		flip()
 	end
